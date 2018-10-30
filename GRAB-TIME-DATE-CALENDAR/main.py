2c63900b4b4c421caf94438ff09dccbb339275b3
Larson Carter <larsoncarter@blazermail.net>
2018-10-30 14:52:23 -0500

    Update main.py

--- GRAB-TIME-DATE-CALENDAR/main.py
+++ GRAB-TIME-DATE-CALENDAR/main.py
@@ -1,13 +1,10 @@
 import datetime
-
 now = datetime.datetime.now()
-
 print("It is:")
-
 print(now.year)
 
 if (now.month) == 1:
-    print("January")   
+    print("January")
 elif (now.month) == 2:
     print("Feburary")
 elif (now.month) == 3:
@@ -21,18 +18,17 @@ elif (now.month) == 6:
 elif (now.month) == 7:
     print("July")
 elif (now.month) == 8:
-    print("August") 
+    print("August")
 elif (now.month) == 9:
     print("September")
 elif (now.month) == 10:
     print("October")
 elif (now.month) == 11:
-    print("November")  
+    print("November")
 elif (now.month) == 12:
     print("December")
 else:
     print("Error!")
-    
 print(now.day)
 print(now.hour)
 print(now.minute)
