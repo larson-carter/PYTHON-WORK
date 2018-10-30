944c2288c007bdd0db890caf790e86c3411a528f
Larson Carter <larsoncarter@blazermail.net>
2018-10-30 10:39:55 -0500

    Create main.py

---
+++ GRAB-TIME-DATE-CALENDAR/main.py
@@ -0,0 +1,38 @@
+import datetime
+
+now = datetime.datetime.now()
+
+# HERE IS THE INDIVITUAL PUT TOGETHER METHODS
+
+"""
+print("Current year: %d" % now.year)
+
+print("Current month: %d" % now.month)
+
+print("Current day: %d" % now.day)
+
+print("Current hour: %d" % now.hour)
+
+print("Current minute: %d" % now.minute)
+
+print("Current second: %d" % now.second)
+
+print("Current microsecond: %d" % now.microsecond)
+"""
+
+"""
+print("Current date and time using strftime:")
+
+print(now.strftime("%Y-%m-%d %H:%M"))
+
+"""
+
+print("It is:")
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+print(now.microsecond)
+print("END OF PROGRAM")
