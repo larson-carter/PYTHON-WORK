7664bb6650ba551e5d3e6343a3cc57b4248422a6
Larson Carter <larsoncarter@blazermail.net>
2018-11-01 14:46:02 -0500

    Update main.py

--- GRAB-TIME-DATE-CALENDAR/main.py
+++ GRAB-TIME-DATE-CALENDAR/main.py
@@ -1,91 +1,94 @@
 import datetime
-now = datetime.datetime.now()
-print(now.year)
-if (now.month) == 1:
+currentDate = datetime.datetime.now()
+year = currentDate.year
+if (currentDate.month) == 1:
     month = "January"
-elif (now.month) == 2:
+elif (currentDate.month) == 2:
     month = "February"
-elif (now.month) == 3:
+elif (currentDate.month) == 3:
     month = "March"
-elif (now.month) == 4:
+elif (currentDate.month) == 4:
     month = "April"
-elif (now.month) == 5:
+elif (currentDate.month) == 5:
     month = "May"
-elif (now.month) == 6:
+elif (currentDate.month) == 6:
     month = "June"
-elif (now.month) == 7:
+elif (currentDate.month) == 7:
     month = "July"
-elif (now.month) == 8:
+elif (currentDate.month) == 8:
     month = "August"
-elif (now.month) == 9:
+elif (currentDate.month) == 9:
     month = "September"
-elif (now.month) == 10:
+elif (currentDate.month) == 10:
     month = "October"
-elif (now.month) == 11:
+elif (currentDate.month) == 11:
     month = "November"
-elif (now.month) == 12:
+elif (currentDate.month) == 12:
     month = "December"
-if now.day == 1:
+if currentDate.day == 1:
     day = "1st"
-if now.day == 2:
+elif currentDate.day == 2:
     day = "2nd"
-if now.day == 3:
+elif currentDate.day == 3:
     day = "3rd"
-if now.day == 4:
+elif currentDate.day == 4:
     day = "4th"
-if now.day == 5:
+elif currentDate.day == 5:
     day = "5th"
-if now.day == 6:
+elif currentDate.day == 6:
     day = "6th"
-if now.day == 7:
+elif currentDate.day == 7:
     day = "7th"
-if now.day == 8:
+elif currentDate.day == 8:
     day = "8th"
-if now.day == 9:
+elif currentDate.day == 9:
     day = "9th"
-if now.day == 10:
+elif currentDate.day == 10:
     day = "10th"
-if now.day == 11:
+elif currentDate.day == 11:
     day = "11th"
-if now.day == 12:
+elif currentDate.day == 12:
     day = "12th"
-if now.day == 13:
+elif currentDate.day == 13:
     day = "13th"
-if now.day == 14:
+elif currentDate.day == 14:
     day = "14th"
-if now.day == 15:
+elif currentDate.day == 15:
     day = "15th"
-if now.day == 16:
+elif currentDate.day == 16:
     day = "16th"
-if now.day == 17:
+elif currentDate.day == 17:
     day = "17th"
-if now.day == 18:
+elif currentDate.day == 18:
     day = "18th"
-if now.day == 19:
+elif currentDate.day == 19:
     day = "19th"
-if now.day == 20:
+elif currentDate.day == 20:
     day = "20th"
-if now.day == 21:
+elif currentDate.day == 21:
     day = "21st"
-if now.day == 22:
+elif currentDate.day == 22:
     day = "22nd"
-if now.day == 23:
+elif currentDate.day == 23:
     day = "23rd"
-if now.day == 24:
+elif currentDate.day == 24:
     day = "24th"
-if now.day == 25:
+elif currentDate.day == 25:
     day = "25th"
-if now.day == 26:
+elif currentDate.day == 26:
     day = "26th"
-if now.day == 27:
+elif currentDate.day == 27:
     day = "27th"
-if now.day == 28:
+elif currentDate.day == 28:
     day = "28th"
-if now.day == 29:
+elif currentDate.day == 29:
     day = "29th"
-if now.day == 30:
+elif currentDate.day == 30:
     day = "30th"
-if now.day == 31:
+elif currentDate.day == 31:
     day = "31st"
-print(month, ":", now.hour, ":", now.minute, ":", now.second)
-print("END-OF-PROGRAM")
+colon = ":"
+hour = currentDate.hour
+minute = currentDate.minute
+second = currentDate.second
+print(year, colon, month, colon, day, colon, hour, colon, minute, colon, second)
