416cd815e823ac17a45c9426ea24c762395e5f25
Larson Carter <larsoncarter@blazermail.net>
2018-11-01 13:50:41 -0500

    Update main.py

--- GRAB-TIME-DATE-CALENDAR/main.py
+++ GRAB-TIME-DATE-CALENDAR/main.py
@@ -1,8 +1,6 @@
 import datetime
 now = datetime.datetime.now()
-print("It is:")
 print(now.year)
-
 if (now.month) == 1:
     print("January")
 elif (now.month) == 2:
@@ -29,7 +27,68 @@ elif (now.month) == 12:
     print("December")
 else:
     print("Error!")
-print(now.day)
+if now.day == 1:
+    print("1st")
+if now.day == 2:
+    print("2nd")
+if now.day == 3:
+    print("3rd")
+if now.day == 4:
+    print("4th")
+if now.day == 5:
+    print("5th")
+if now.day == 6:
+    print("6th")
+if now.day == 7:
+    print("7th")
+if now.day == 8:
+    print("8th")
+if now.day == 9:
+    print("9th")
+if now.day == 10:
+    print("10th")
+if now.day == 11:
+    print("11th")
+if now.day == 12:
+    print("12th")
+if now.day == 13:
+    print("13ht")
+if now.day == 14:
+    print("14ht")
+if now.day == 15:
+    print("15th")
+if now.day == 16:
+    print("16th")
+if now.day == 17:
+    print("17th")
+if now.day == 18:
+    print("18th")
+if now.day == 19:
+    print("19th")
+if now.day == 20:
+    print("20th")
+if now.day == 21:
+    print("21st")
+if now.day == 22:
+    print("22nd")
+if now.day == 23:
+    print("23rd")
+if now.day == 24:
+    print("24th")
+if now.day == 25:
+    print("25th")
+if now.day == 26:
+    print("26th")
+if now.day == 27:
+    print("27th")
+if now.day == 28:
+    print("28th")
+if now.day == 29:
+    print("29th")
+if now.day == 30:
+    print("30th")
+if now.day == 31:
+    print("31st")
 print(now.hour)
 print(now.minute)
 print(now.second)
